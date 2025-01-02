ce8839028d00bb67aff4c0d157f03252a40bccd5
Blessen25 <blessenvinoymathew25@gmail.com>
2025-01-02 11:20:17 +0530

    Merged with parent

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -13,14 +13,25 @@ const Projects = () => {
                         <div className="project-parentdiv">
                             <div className="project-child">
                                 <h1>Project 1</h1>
+                                <div className="projectinnerchild">
+                                    <img src="../Images/plantbased.png" alt="" className="projectimgsrc" />
+                                </div>
                             </div>
                             <div className="project-child">
                                 <h1>Project 2</h1>
-
+                                <div className="projectinnerchild">
+                                    <img src="../Images/Clothshop.png" alt="" className="projectimgsrc" />
+                                </div>
                             </div>
                             <div className="project-child">
                                 <h1>Project 3</h1>
+                                <div className="projectinnerchild">
+                                    <img src="../Images/plantbased.png" alt="" className="projectimgsrc" />
+                                </div>
                             </div>
+                        </div>
+                        <div className='viewallprojects'>
+                            <a href='#' className='cstm-projects-btn'>View Projects</a>
                         </div>
                     </>
                 }
