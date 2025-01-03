927996b97d2a7dd748b65c7b8703ee441985fe8a
Blessen25 <blessenvinoymathew25@gmail.com>
2025-01-03 18:01:21 +0530

    Commited Changes

--- src/components/contactme/contact.tsx
+++ src/components/contactme/contact.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import './contact.css'
 import { SectionComponent, Subtitleandtitle } from "../extraComponents/Components.tsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 
-const Contactme = () =>{
-    return(
+const Contactme = () => {
+    return (
         <SectionComponent id={"Contact"} child={
             <>
-            <Subtitleandtitle subtitle="Get In Touch" title="Contact Me"/>
-            <div className="contact-parentdiv">
-                <div className="contact-childdiv">
-                    
+                <Subtitleandtitle subtitle="Get In Touch" title="Contact Me" />
+                <div className="contact-parentdiv">
+                    <div className="contact-childdiv">
+                        <div className="contact-innerchild">
+                            <div className="contact-innerrounddiv">
+                                <FontAwesomeIcon icon={faEnvelope} className="mailclass-cstm" />
+                            </div>
+                            <a href="mailto:blessenvinoymathew25@gmail.com" className="contact-atag">blessenvinoymathew25@gmail.com</a>
+                        </div>
+                        <div className="contact-innerchild">
+                            <div className="contact-innerrounddiv">
+                                <FontAwesomeIcon icon={faPhone} className="mailclass-cstm" />
+                            </div>
+                            <a href="tel:+919074493076" className="contact-atag">9074493076</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </>
         } />
     )
