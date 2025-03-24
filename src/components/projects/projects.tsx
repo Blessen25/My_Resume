83d9d206ba442ac1bf6d784f268c67ccf90e7110
Blessen25 <blessenvinoymathew25@gmail.com>
2025-03-24 17:47:35 +0530

    Commited Changes

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SectionComponent, Subtitleandtitle } from '../extraComponents/Components.tsx';
 import './projects.css'
 import { ProjectCardDivProps } from '../../interface/interface.tsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 
 export const Projects = () => {
+
+    const [ismodalopen, setIsModalOpen] = useState(false);
+
+    const handleModalOpen = () => {
+
+        setIsModalOpen(true);
+        document.body.style.overflow = 'hidden';
+    }
+
+    const handleModalClose = () => {
+
+        setIsModalOpen(false);
+        document.body.style.overflow = 'auto';
+    }
+
     return (
         <>
             <SectionComponent
@@ -12,16 +29,28 @@ export const Projects = () => {
                     <>
                         <Subtitleandtitle subtitle='Browse My Recent' title='Projects' />
                         <div className="project-parentdiv">
-                            <ProjectCardDiv projectimg='../Images/Shop.cloth.png' projectno='Project 1' projecthref='https://clothappbvm.vercel.app/'/>
-                            <ProjectCardDiv projectimg='../Images/expense_tracker.png' projectno='Project 2' projecthref='https://expense-tracker-bvm.vercel.app/'/>
+                            <ProjectCardDiv projectimg='../Images/Shop.cloth.png' projectno='Project 1' projecthref='https://clothappbvm.vercel.app/' />
+                            <ProjectCardDiv projectimg='../Images/expense_tracker.png' projectno='Project 2' projecthref='https://expense-tracker-bvm.vercel.app/' />
                             <ProjectCardDiv projectimg='../Images/Plantbasedapp.png' projectno='Project 3' />
-                            <ProjectCardDiv projectimg='../Images/instagramandfacebookclone.png' projectno='Project 4' />
-                            <ProjectCardDiv projectimg='../Images/instagram.png' projectno='Project 5' />
-                            <ProjectCardDiv projectimg='../Images/Resume_portfolio.png' projectno='Project 6' projecthref='https://blessenportfolio.vercel.app/'/>
                         </div>
-                        {/* <div className='viewallprojects'>
-                            <a href='#' className='cstm-projects-btn'>View Projects</a>
-                        </div> */}
+                        <div className='viewallprojects'>
+                            <a href='#' className='cstm-projects-btn' onClick={handleModalOpen}>View Projects</a>
+                        </div>
+                        {ismodalopen && (
+                            <div className="modal-overlay">
+                                <div className="modal-content">
+                                    <div className='close-modal-icondiv'><FontAwesomeIcon icon={faClose} onClick={handleModalClose} /></div>
+                                    <div className="product-modalcstm">
+                                        <MoreProjectCardDiv projectimg='../Images/Shop.cloth.png' projectno='Project 1' projecthref='https://clothappbvm.vercel.app/' />
+                                        <MoreProjectCardDiv projectimg='../Images/expense_tracker.png' projectno='Project 2' projecthref='https://expense-tracker-bvm.vercel.app/' />
+                                        <MoreProjectCardDiv projectimg='../Images/Plantbasedapp.png' projectno='Project 3' />
+                                        <MoreProjectCardDiv projectimg='../Images/instagramandfacebookclone.png' projectno='Project 4' />
+                                        <MoreProjectCardDiv projectimg='../Images/instagram.png' projectno='Project 5' />
+                                        <MoreProjectCardDiv projectimg='../Images/Resume_portfolio.png' projectno='Project 6' projecthref='https://blessenportfolio.vercel.app/' />
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </>
                 }
             />
@@ -29,24 +58,48 @@ export const Projects = () => {
     )
 }
 
-export const ProjectCardDiv:React.FC<ProjectCardDivProps> = (Props) =>{
+export const ProjectCardDiv: React.FC<ProjectCardDivProps> = (Props) => {
 
-    return(
+    return (
         <>
             <div className="project-child">
                 <h1>{Props.projectno}</h1>
                 <div className="projectinnerchild">
                     {Props.projecthref ? (
                         <>
-                            <a href={Props.projecthref} style={{ cursor : 'pointer'}} target='_blank' >
+                            <a href={Props.projecthref} style={{ cursor: 'pointer' }} target='_blank' >
                                 <img src={Props.projectimg} alt="" className="projectimgsrc" />
                             </a>
                         </>
                     ) : (
                         <>
-                            <img src={Props.projectimg} alt="" className="projectimgsrc" /> 
+                            <img src={Props.projectimg} alt="" className="projectimgsrc" />
                         </>
-                    ) }
+                    )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export const MoreProjectCardDiv: React.FC<ProjectCardDivProps> = (Props) => {
+
+    return (
+        <>
+            <div className="project-child1">
+                <h1>{Props.projectno}</h1>
+                <div className="projectinnerchild">
+                    {Props.projecthref ? (
+                        <>
+                            <a href={Props.projecthref} style={{ cursor: 'pointer' }} target='_blank' >
+                                <img src={Props.projectimg} alt="" className="projectimgsrc1" />
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <img src={Props.projectimg} alt="" className="projectimgsrc1" />
+                        </>
+                    )}
                 </div>
             </div>
         </>
